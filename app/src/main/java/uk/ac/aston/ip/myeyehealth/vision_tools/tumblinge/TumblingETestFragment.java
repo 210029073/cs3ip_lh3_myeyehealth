50aa9a6bf5c02b4ebbef0219e1143f2fc1089aad
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-12-23 20:30:03 +0000

    Updated Tumbling E fragment now shows the letter E with the font size and rotation set at random

--- app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/tumblinge/TumblingETestFragment.java
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/tumblinge/TumblingETestFragment.java
@@ -1,7 +1,7 @@
 package uk.ac.aston.ip.myeyehealth.vision_tools.tumblinge;
 
 import androidx.lifecycle.ViewModelProvider;
-
+import java.util.Random;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,10 +13,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import uk.ac.aston.ip.myeyehealth.R;
+import uk.ac.aston.ip.myeyehealth.databinding.FragmentTumblingETestBinding;
 
 public class TumblingETestFragment extends Fragment {
 
     private TumblingETestViewModel mViewModel;
+
+    private FragmentTumblingETestBinding binding;
 
     public static TumblingETestFragment newInstance() {
         return new TumblingETestFragment();
@@ -25,7 +28,30 @@ public class TumblingETestFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tumbling_e_test, container, false);
+//        return inflater.inflate(R.layout.fragment_tumbling_e_test, container, false);
+        binding = FragmentTumblingETestBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        int[] letterE = randomManipulateE();
+    }
+
+    private int[] randomManipulateE() {
+        Random random = new Random();
+        int[] randomRotate = {0, 90, 180, 270, 360};
+        int[] randomSize = {12, 16,28, 30, 36, 48, 72, 108};
+        int letterEAngleRotation = randomRotate[random.nextInt(randomRotate.length - 1)];
+        int letterESize = randomSize[random.nextInt(randomSize.length - 1)];
+
+        binding.txtLetterE.setTextAppearance(0);
+        binding.txtLetterE.setRotation(letterEAngleRotation);
+        binding.txtLetterE.setTextSize(letterESize);
+
+        return new int[]{letterESize, letterEAngleRotation};
     }
 
     @Override
