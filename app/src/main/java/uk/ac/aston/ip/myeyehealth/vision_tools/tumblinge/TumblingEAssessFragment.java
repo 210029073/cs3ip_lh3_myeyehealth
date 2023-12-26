c0a444cd53a4499e1ed661a2a2ac032683a053f0
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-12-26 12:19:30 +0000

    Created interactive tumbling E game, Created snackbar for providing feedback if the user has selected the correct answer.

--- app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/tumblinge/TumblingEAssessFragment.java
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/tumblinge/TumblingEAssessFragment.java
@@ -2,6 +2,7 @@ package uk.ac.aston.ip.myeyehealth.vision_tools.tumblinge;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,11 +13,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import uk.ac.aston.ip.myeyehealth.R;
+import uk.ac.aston.ip.myeyehealth.databinding.FragmentTumblingEAssessBinding;
 
 public class TumblingEAssessFragment extends Fragment {
 
     private TumblingEAssessViewModel mViewModel;
+
+    private TumblingETestViewModel viewModel;
+
+    private FragmentTumblingEAssessBinding binding;
 
     public static TumblingEAssessFragment newInstance() {
         return new TumblingEAssessFragment();
@@ -25,7 +33,100 @@ public class TumblingEAssessFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tumbling_e_assess, container, false);
+        binding = FragmentTumblingEAssessBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //TODO: get data from view model
+        this.viewModel = new ViewModelProvider(requireActivity()).get(TumblingETestViewModel.class);
+        int[] letterPosSize = this.viewModel.getCurrentLetterEPosSize().getValue();
+
+        int size = letterPosSize[0];
+        int position = letterPosSize[1];
+
+        binding.option1Card.setOnClickListener(optionSelected -> {
+            int parsedSize = (int) binding.option1.getTextSize();
+            int angle = (int) binding.option1.getRotation();
+            if (angle == position) {
+                Snackbar.make(getView(), "Correct answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_1_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.green))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+
+            else {
+                Snackbar.make(getView(), "Incorrect answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_1_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.red))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+        });
+
+        binding.option2Card.setOnClickListener(optionSelected -> {
+            int parsedSize = (int) binding.option2.getTextSize();
+            int angle = (int) binding.option2.getRotation();
+            if (angle == position) {
+                Snackbar.make(getView(), "Correct answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_2_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.green))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+
+            else {
+                Snackbar.make(getView(), "Incorrect answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_2_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.red))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+        });
+
+        binding.option3Card.setOnClickListener(optionSelected -> {
+            int parsedSize = (int) binding.option3.getTextSize();
+            int angle = (int) binding.option3.getRotation();
+            if (angle == position) {
+                Snackbar.make(getView(), "Correct answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_3_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.green))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+
+            else {
+                Snackbar.make(getView(), "Incorrect answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_3_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.red))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+        });
+
+        binding.option4Card.setOnClickListener(optionSelected -> {
+            int parsedSize = (int) binding.option4.getTextSize();
+            int angle = (int) binding.option4.getRotation();
+            if (angle == position) {
+                Snackbar.make(getView(), "Correct answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_4_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.green))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+
+            else {
+                Snackbar.make(getView(), "Incorrect answer", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.option_4_card)
+                        .setBackgroundTintList(getContext().getColorStateList(R.color.red))
+                        .setTextColor(getContext().getColorStateList(R.color.white))
+                        .setAction("Action",null).show();
+            }
+        });
     }
 
     @Override
