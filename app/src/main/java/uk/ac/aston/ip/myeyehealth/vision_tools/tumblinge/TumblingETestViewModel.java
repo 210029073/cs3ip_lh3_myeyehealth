8abe49b58355ae9a55f77b5781810565097b297b
Ibrahim Ahmad <210029073@aston.ac.uk>
2023-12-26 10:13:55 +0000

    Updated Tumbling E test quiz with options represented onscreen.

--- app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/tumblinge/TumblingETestViewModel.java
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/tumblinge/TumblingETestViewModel.java
@@ -8,20 +8,30 @@ public class TumblingETestViewModel extends ViewModel {
     // TODO: Implement the ViewModel
 
     //TODO: Need to separate score for right and left eyes
-    private MutableLiveData<Integer> score;
+    private MutableLiveData<Integer> leftEyeScore;
+
+    private MutableLiveData<Integer> rightEyeScore;
 
     private MutableLiveData<Integer[]> currentLetterEPosSize;
 
-    public void setScore(int score) {
-        this.score.setValue(score);
+    public void setLeftEyeScore(int score) {
+        this.leftEyeScore.setValue(score);
+    }
+
+    public void setRightEyeScore(int score) {
+        this.rightEyeScore.setValue(score);
     }
 
     public void setCurrentLetterEPosSize(Integer[] ePosSize) {
         this.currentLetterEPosSize.setValue(ePosSize);
     }
 
-    public LiveData<Integer> getScore() {
-        return this.score;
+    public LiveData<Integer> getLeftEyeScore() {
+        return this.leftEyeScore;
+    }
+
+    public LiveData<Integer> getRightEyeScore() {
+        return this.rightEyeScore;
     }
 
     public LiveData<Integer[]> getCurrentLetterEPosSize() {
