7b9a8cd300abf4bae688e107bf0a1f4ab8142389
Ibrahim Ahmad <210029073@aston.ac.uk>
2024-01-17 15:58:13 +0000

    Created additional database schema for storing test scores

---
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/doa/TestRecordDAO.java
@@ -0,0 +1,34 @@
+package uk.ac.aston.ip.myeyehealth.doa;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import uk.ac.aston.ip.myeyehealth.entities.TestRecord;
+
+@Dao
+public interface TestRecordDAO {
+    @Query("SELECT * FROM testrecord")
+    List<TestRecord> getAll();
+
+    @Insert
+    void insertTestRecord(TestRecord record);
+
+    @Insert
+    void insertAllTestRecords(TestRecord ... record);
+
+
+    @Update
+    void updateTestRecord(TestRecord old, TestRecord update);
+
+    @Delete
+    void deleteAllRecords(TestRecord ... testRecords);
+
+    @Delete
+    void deleteRecord(TestRecord testRecord);
+
+}
---
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/entities/TestRecord.java
@@ -0,0 +1,21 @@
+package uk.ac.aston.ip.myeyehealth.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class TestRecord {
+    @PrimaryKey(autoGenerate = true)
+    public int testRecordNo;
+
+    @ColumnInfo(name = "test_result_score")
+    public float testResultScore;
+
+    @ColumnInfo(name = "test_result_description")
+    public String testResultDescription;
+
+    @ColumnInfo(name = "test_result_record_time")
+    public long testResultRecordTime;
+
+}
