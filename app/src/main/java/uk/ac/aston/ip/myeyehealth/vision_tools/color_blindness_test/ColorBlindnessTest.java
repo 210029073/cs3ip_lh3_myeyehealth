7822b906c18414de1ffff20af6d07437052be529
Ibrahim Ahmad <210029073@aston.ac.uk>
2024-02-05 13:19:36 +0000

    Updated color blind test in order to update scores, and keep track of the number of tries.

--- app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/color_blindness_test/ColorBlindnessTest.java
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/color_blindness_test/ColorBlindnessTest.java
@@ -7,10 +7,17 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Random;
 
 import uk.ac.aston.ip.myeyehealth.R;
 import uk.ac.aston.ip.myeyehealth.databinding.FragmentColorBlindnessTestBinding;
@@ -25,6 +32,8 @@ public class ColorBlindnessTest extends Fragment {
         return new ColorBlindnessTest();
     }
 
+    private int imageResource = 0;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -35,18 +44,103 @@ public class ColorBlindnessTest extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.mViewModel = new ColorBlindnessTestViewModel();
+//        this.mViewModel = new ViewModelProvider(requireActivity()).get(ColorBlindnessTestViewModel.class);
+        Log.d("ColorBlindTest", "onViewCreated: " + mViewModel.getScore());
 
         ColorBlindQuestionGenerator colorBlindQuestionGenerator = new ColorBlindQuestionGenerator();
-        int imageResource = colorBlindQuestionGenerator.getRandomColorBlindQuestionResource();
-        binding.colorBlindnessImageview.setImageResource(imageResource);
+        generateRandomQuestion(colorBlindQuestionGenerator);
+        binding.colorBlindnessImageview.setImageResource(this.imageResource);
 
-        binding.colorBlindnessOptionA.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            Log.d("ColorBlindTest", "onViewCreated: " + mViewModel.getNumberOfTries());
 
-        binding.colorBlindnessOptionB.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
 
-        binding.colorBlindnessOptionC.setText(String.valueOf(colorBlindQuestionGenerator.findKey(imageResource)));
+            binding.colorBlindnessOptionA.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            binding.colorBlindnessOptionA.setOnClickListener(v -> {
 
-        binding.colorBlindnessOptionD.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+                if (colorBlindQuestionGenerator.findKey(this.imageResource).equals(binding.colorBlindnessOptionA.getText())) {
+                    int tmp = mViewModel.getScore() + 1;
+                    mViewModel.score.setValue(tmp);
+                }
+                mViewModel.numberOfTries.setValue(mViewModel.getNumberOfTries() + 1);
+                generateRandomQuestion(colorBlindQuestionGenerator);
+                Log.d("ColorBlindTest", "onViewCreated: " + mViewModel.getScore());
+
+            });
+
+            binding.colorBlindnessOptionB.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            binding.colorBlindnessOptionB.setOnClickListener(v -> {
+
+                if (colorBlindQuestionGenerator.findKey(this.imageResource).equals(binding.colorBlindnessOptionB.getText())) {
+                    int tmp = mViewModel.getScore() + 1;
+                    mViewModel.score.setValue(tmp);
+                }
+                mViewModel.numberOfTries.setValue(mViewModel.getNumberOfTries() + 1);
+                generateRandomQuestion(colorBlindQuestionGenerator);
+                Log.d("ColorBlindTest", "onViewCreated: " + mViewModel.getScore());
+
+            });
+
+            binding.colorBlindnessOptionC.setText(String.valueOf(colorBlindQuestionGenerator.findKey(this.imageResource)));
+            binding.colorBlindnessOptionC.setOnClickListener(v -> {
+
+                if (colorBlindQuestionGenerator.findKey(imageResource).equals(binding.colorBlindnessOptionC.getText())) {
+                    int tmp = mViewModel.getScore() + 1;
+                    mViewModel.score.setValue(tmp);
+                }
+                mViewModel.numberOfTries.setValue(mViewModel.getNumberOfTries() + 1);
+                generateRandomQuestion(colorBlindQuestionGenerator);
+                Log.d("ColorBlindTest", "onViewCreated: " + mViewModel.getScore());
+
+            });
+
+            binding.colorBlindnessOptionD.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            binding.colorBlindnessOptionD.setOnClickListener(v -> {
+
+                if (colorBlindQuestionGenerator.findKey(this.imageResource).equals(binding.colorBlindnessOptionD.getText())) {
+                    int tmp = mViewModel.getScore() + 1;
+                    mViewModel.score.setValue(tmp);
+                }
+                mViewModel.numberOfTries.setValue(mViewModel.getNumberOfTries() + 1);
+                generateRandomQuestion(colorBlindQuestionGenerator);
+                Log.d("ColorBlindTest", "onViewCreated: " + mViewModel.getScore());
+            });
+
+    }
+
+    private void generateRandomQuestion(ColorBlindQuestionGenerator colorBlindQuestionGenerator) {
+        if(mViewModel.getNumberOfTries() < 5) {
+
+
+            this.imageResource = colorBlindQuestionGenerator.getRandomColorBlindQuestionResource();
+
+            binding.colorBlindnessImageview.setImageResource(this.imageResource);
+
+            //Choose a button at random to hold the answer
+            int[] button_ids = new int[4];
+            button_ids[0] = binding.colorBlindnessOptionA.getId();
+            button_ids[1] = binding.colorBlindnessOptionB.getId();
+            button_ids[2] = binding.colorBlindnessOptionC.getId();
+            button_ids[3] = binding.colorBlindnessOptionD.getId();
+
+            final int chosenId = new Random().nextInt(button_ids.length - 1);
+
+            binding.colorBlindnessOptionA.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            binding.colorBlindnessOptionB.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            binding.colorBlindnessOptionC.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+            binding.colorBlindnessOptionD.setText(String.valueOf(colorBlindQuestionGenerator.generateRandomCombination()));
+
+            View view = getView().findViewById(button_ids[chosenId]);
+            if (view instanceof Button) {
+                ((Button) view).setText(colorBlindQuestionGenerator.findKey(this.imageResource));
+            }
+        }
+        else {
+            Bundle bundle = new Bundle();
+            bundle.putInt("score", mViewModel.getScore());
+            NavHostFragment.findNavController(ColorBlindnessTest.this)
+                    .navigate(R.id.action_colorBlindnessTest_to_colorBlindnessTestScore2, bundle);
+        }
     }
 
     @Override
--- app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/color_blindness_test/ColorBlindnessTestViewModel.java
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/color_blindness_test/ColorBlindnessTestViewModel.java
@@ -1,7 +1,24 @@
 package uk.ac.aston.ip.myeyehealth.vision_tools.color_blindness_test;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ColorBlindnessTestViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    MutableLiveData<Integer> score;
+
+    MutableLiveData<Integer> numberOfTries;
+
+    public ColorBlindnessTestViewModel() {
+        this.score = new MutableLiveData<>(0);
+        this.numberOfTries = new MutableLiveData<>(0);
+    }
+
+    public int getScore() {
+        return score.getValue();
+    }
+
+    public int getNumberOfTries() {
+        return numberOfTries.getValue();
+    }
 }
