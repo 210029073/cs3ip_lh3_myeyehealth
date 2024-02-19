b49f2911ea55956c7c45b3d0a841cde9c7c0666d
Ibrahim Ahmad <210029073@aston.ac.uk>
2024-02-19 16:00:19 +0000

    Added functionality for performing metric conversion for test scores, and updated missing labels for fragments in Nav Graph.

--- app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/metric_conversion/MetricConversionFragment.java
+++ app/src/main/java/uk/ac/aston/ip/myeyehealth/vision_tools/metric_conversion/MetricConversionFragment.java
@@ -2,13 +2,23 @@ package uk.ac.aston.ip.myeyehealth.vision_tools.metric_conversion;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import uk.ac.aston.ip.myeyehealth.R;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import uk.ac.aston.ip.myeyehealth.databinding.FragmentMetricConversionBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +31,8 @@ public class MetricConversionFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public FragmentMetricConversionBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +73,80 @@ public class MetricConversionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_metric_conversion, container, false);
+        this.binding = FragmentMetricConversionBinding.inflate(inflater, container, false);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //get the users text input
+        MutableLiveData<Integer> testingDistance = new MutableLiveData<>(0);
+        MutableLiveData<Integer> distance = new MutableLiveData<>(0);
+
+        //this gets the textview for storing the outputs for the unit conversions
+        TextView testScoreOutput = binding.visualAcuityScore;
+        //this stores the default output
+        String defaultTestScoreOutput = testScoreOutput.getText().toString();
+
+        MutableLiveData<String> testScoreOutputStr = new MutableLiveData<>("");
+
+        //this will change the testScoreOutput TextView object when the user modifies the testingDistance
+        binding.testingDistance.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //get the users text input
+                Log.d("MetricConversionFragment", "onTextChanged: " + s);
+                if(s.length() == 0) {
+                    testScoreOutputStr.setValue(0 + "/" + distance.getValue());
+                    testScoreOutput.setText(testScoreOutputStr.getValue());
+                }
+                else {
+                    testingDistance.setValue(Integer.parseInt(binding.testingDistance.getEditText().getText().toString()));
+                    int testDistanceFeet = (int) Integer.valueOf(testingDistance.getValue() * 10/3);
+                    int distanceFeet = (int) Integer.valueOf(distance.getValue() * 10/3);
+                    testScoreOutputStr.setValue(testDistanceFeet + "/" + distanceFeet);
+                    testScoreOutput.setText(testScoreOutputStr.getValue());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.distance.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.equals("")) {
+                    testScoreOutputStr.setValue(testingDistance.getValue() + "/" + 0);
+                }
+                else {
+                    distance.setValue(Integer.parseInt(binding.distance.getEditText().getText().toString()));
+                    int testDistanceFeet = (int) Integer.valueOf(testingDistance.getValue() * 10/3);
+                    int distanceFeet = (int) Integer.valueOf(distance.getValue() * 10/3);
+                    testScoreOutputStr.setValue(testDistanceFeet + "/" + distanceFeet);
+                    testScoreOutput.setText(testScoreOutputStr.getValue());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
